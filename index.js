167250a29a8294e44145dbe803758714e55ad2dc
hamesomer11 <hamzaomer45@gmail.com>
2024-07-18 22:44:09 +0100

    Phonebook backend: Exercise 3.9 - commit #3

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
 
+app.use(express.static('dist'))
 app.use(cors())
+
+
 
 morgan.token('body', function (req) {
     return JSON.stringify(req.body)
