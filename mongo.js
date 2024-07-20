5668e34f475725e30a10fcb9d633aad7b29218a5
hamesomer11 <hamzaomer45@gmail.com>
2024-07-20 21:47:55 +0100

    Phonebook backend: Exercise 3.12 - commit #5

--- mongo.js
+++ mongo.js
@@ -39,9 +39,9 @@ if (process.argv.length === 3) {
         name: process.argv[3],
         number: process.argv[4],
     })
-}
 
-person.save().then(result => {
-  console.log('person saved!')
-  mongoose.connection.close()
-})
+    person.save().then(result => {
+        console.log('person saved!')
+        mongoose.connection.close()
+    });
+}
