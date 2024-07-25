fb6b0c750641664e169828bbad542fc10b846bb1
hamesomer11 <hamzaomer45@gmail.com>
2024-07-25 13:30:20 +0100

    Deploying the database backend to production - Exercise 3.21 - commit #4

--- index.js
+++ index.js
@@ -101,7 +101,7 @@ app.get('/api/persons/:id', (request, response, next) => {
 
 app.delete('/api/persons/:id', (request, response, next) => {
     console.log(request.params.id.toString())
-      Person.findByIdAndDelete(request.params.id.toString())
+      Person.findByIdAndDelete(request.params.id)
       .then(result => {
         response.status(204).end()
       })
